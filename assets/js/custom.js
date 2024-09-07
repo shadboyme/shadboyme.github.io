dec46d39d4f1bf2e093c185275edb71909fd1177
~$ Nixton Cipher <83667327+nixtondev@users.noreply.github.com>
2024-09-07 15:31:15 +0200

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,40 @@
-        var copy = "&copy;";
-        var author = " nixton.dev ";
+var copy = "&copy;";
+var author = " nixton.dev ";
 
-        function getCopy(year){
+function getCopy(year){
   
-        document.write(copy);
-        document.write(author);
-        document.write(year, ' - ');
-        document.write(new Date().getFullYear());
+document.write(copy);
+document.write(author);
+document.write(year, ' - ');
+document.write(new Date().getFullYear());
   
-        }
+}
+
+function getLast() {
+
+const birthday = 1997;
+const currentYear = new Date().getFullYear();
+const age = currentYear - birthday;
+const lifeEnd = 100;
+const yearsLeft = lifeEnd - age;
+
+document.getElementById("end").textContent = "" + yearsLeft;
+}
+
+getLast();
+
+function getAge(birthDate) {
+
+const today = new Date();
+const birth = new Date(birthDate);
+let age = today.getFullYear() - birth.getFullYear();
+
+if (today < new Date(today.getFullYear(), birth.getMonth(), birth.getDate())) {
+age--;
+}
+
+return age;
+
+}
+
+window.onload = () => document.getElementById("start").textContent = "" + getAge("1997-02-13");
